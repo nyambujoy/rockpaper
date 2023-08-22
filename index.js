5782236057ba15b1a38d849f4ca014d31a60b91f
nyambujoy <joyliznyambura@gmail.com>
2023-08-22 10:41:44 +0300

    added the game logic

--- index.js
+++ index.js
@@ -6,8 +6,38 @@
 
 function computerChoice(){
     var compChoice =["rock","paper","scissors"]
-    let randomChoice = compChoice[Math.floor(Math.random()*compChoice.length)]
-    console.log(randomChoice)
+    return randomChoice = compChoice[Math.floor(Math.random()*compChoice.length)]
+    //console.log(randomChoice)
 }
 
+//computerChoice()
+
+let playerChoice= prompt("please pick one :rock,paper,scissors")
+
+if (playerChoice == "rock" ){
+    console.log("you picked:rock ")
+}else if (playerChoice == "paper"){
+    console.log("you picked :paper")
+}else if (playerChoice =="scissors"){
+    console.log("you picked: scissors" )
+}
+
+function oneRound(playerChoice,computerChoice){
+   if (playerChoice === "paper" && computerChoice ==="rock"){
+    console.log(" computer picked rock : you  won")
+   }else if(playerChoice === "scissors" && computerChoice === "paper"){
+    console.log("computer picked paper : You won")
+   }else if (playerChoice === "rock" && computerChoice ==="scissors"){
+    console.log("computer picked scissors : you won")
+   }else if (playerChoice === computerChoice){
+    console.log("you both tied")
+
+   }else {
+    console.log(`computer picked ${computerChoice} computer wins`)
+   
+   }
+}
+
+    
 computerChoice()
+oneRound(playerChoice,computerChoice())
