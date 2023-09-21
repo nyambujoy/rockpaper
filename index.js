71466b1bb632287d396f4dce7a0440a4c4bbb2c7
nyambujoy <joyliznyambura@gmail.com>
2023-09-21 12:17:30 +0300

    added some css

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const computerChoiceDisplay = document.getElementById("computer-choice");//no need for #cuz your using getelementbyid
-const userChoiceDisplay =document.getElementById("user-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result")
+const playAgainDisplay = document.getElementById("playagain")
 const possiblChoices = document.querySelectorAll("button")
+
 let userChoice;
 let computerChoice;
-let compScore=0
-let yourScore=0
+let compScore = 0
+let yourScore = 0
+let playAgain = true
 
-possiblChoices.forEach(possiblChoice =>possiblChoice.addEventListener("click",(e)=>{
-    userChoice= e.target.id
+possiblChoices.forEach(possiblChoice => possiblChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
     resultDisplay.innerHTML = checkWinner()
-   
+    playAgainDisplay.innerHTML = playAgain()
+
 }))
 
-function generateComputerChoice(){
-    randNum=Math.floor(Math.random()* 3)+1
+function generateComputerChoice() {
+    randNum = Math.floor(Math.random() * 3) + 1
     console.log(randNum)
 
-    switch(randNum){
+    switch (randNum) {
         case 1:
             computerChoice = "rock"
             break
@@ -35,22 +39,29 @@ function generateComputerChoice(){
     computerChoiceDisplay.innerHTML = computerChoice
 }
 
-function checkWinner(){
-    let compScore=0
-    let yourScore=0
-    if (computerChoice == userChoice){
+function checkWinner() {
+    let compScore = 0
+    let yourScore = 0
+    if (computerChoice == userChoice) {
         return "Draw!"
-    }else if(computerChoice === "paper" && userChoice === "rock"){
-        compScore+=1
-        return `computer wins ! score : ${compScore }`
-    }else if( computerChoice === "scissors"&& userChoice ==="paper"){
-        compScore+=1
-        return `computer wins ! score : ${compScore}`
-    }else if(computerChoice === "rock" && userChoice === "scissors"){
-        compScore+=1
-        return `computer wins ! score : ${compScore}`
-    }else{
-        yourScore+=1
-        return `you win ! score : ${yourScore}`
+    } else if (computerChoice === "paper" && userChoice === "rock") {
+        compScore++
+        return "computer wins"
+    } else if (computerChoice === "scissors" && userChoice === "paper") {
+        compScore++;
+        return "computer wins"
+
+    } else if (computerChoice === "rock" && userChoice === "scissors") {
+        compScore++
+        return "computer wins"
+
+    } else {
+        yourScore++
+        return "you win"
+
     }
+
+
+
 }
+
